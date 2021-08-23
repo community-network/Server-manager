fb84b22907ad0930df35ad2816348063a73755c8
Zefanja Jobse <zefanjaj@gmail.com>
2021-08-23 13:49:31 +0200

    better graph date markings

--- src/Group/Charts.js
+++ src/Group/Charts.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Pie, Line, Chart } from 'react-chartjs-2';
+import { Pie, Line, Chart } from 'react-chartjs-2';
+import "chartjs-adapter-date-fns";
 import zoomPlugin from "chartjs-plugin-zoom";
 import { useTranslation } from 'react-i18next';
 import { Button } from "../components";
 
-Chart.register(zoomPlugin); 
+Chart.register(zoomPlugin);
 
 export function MapInfo(props) {
     const { t } = useTranslation();
@@ -48,10 +49,10 @@ export function MapInfo(props) {
             },
         ],
     };
-    const options = { 
-        plugins: { 
-            legend: { 
-                display: false 
+    const options = {
+        plugins: {
+            legend: {
+                display: false
             },
         }
     }
@@ -59,9 +60,9 @@ export function MapInfo(props) {
     return (
         <div style={{ display: "block" }}>
             <h5 style={{ textAlign: "center" }}>{t("group.status.stats.servers.map.main")}</h5>
-            <Pie 
-                options={options} 
-                style={{ maxHeight: "250px", maxWidth: "250px", marginLeft: "20px" }} 
+            <Pie
+                options={options}
+                style={{ maxHeight: "250px", maxWidth: "250px", marginLeft: "20px" }}
                 data={data}
             />
         </div>
@@ -74,57 +75,78 @@ export function PlayerInfo(props) {
     const chartRef = React.useRef(null)
     const resetZoom = () => {
         chartRef.current.resetZoom()
-     }
+    }
 
     const time = props.stats.timeStamps.map((e) => {
         const time = new Date(e);
-        return t("chartDateTime", {date: time})
-        // return time.toLocaleDateString();
-      });
-    const options = { 
-        plugins: { 
-            zoom: { 
-                zoom: { 
+        return time;
+    });
+    const options = {
+        scales: {
+            x: {
+                type: "time",
+                ticks: {
+                    autoSkip: true,
+                    autoSkipPadding: 50,
+                    maxRotation: 0,
+                },
+                time: {
+                    displayFormats: {
+                        hour: "HH:mm, eee d LLL",
+                    },
+                },
+            },
+        },
+        plugins: {
+            zoom: {
+                limits: {
+                    x: {
+                        min: +new Date() - 2629800000,
+                        max: +new Date(),
+                        minRange: 10000000,
+                    },
+                },
+                zoom: {
                     mode: "x",
                     drag: { enabled: true },
                     pinch: {
                         enabled: true
                     }
                 },
-            } 
-        } 
+            }
+        }
     };
     const data = {
         labels: time,
         datasets: [
-          {
-            label: t("group.status.stats.servers.info.players"),
-            data: props.stats.playerAmounts,
-            fill: false,
-            backgroundColor: "#4bc0c0",
-            borderColor: 'rgba(75,192,192, 0.2)'
-          },
-          {
-            label: t("group.status.stats.servers.info.vips"),
-            data: props.stats.vipAmounts,
-            fill: false,
-            backgroundColor: "#49297e",
-            borderColor: 'rgba(73, 41, 126, 0.2)'
-          },
-          {
-            label: t("group.status.stats.servers.info.admins"),
-            data: props.stats.adminAmounts,
-            fill: false,
-            backgroundColor: "#195f08",
-            borderColor: 'rgba(25, 95, 8, 0.2)'
-          },
-          {
-            label: t("group.status.stats.servers.info.bans"),
-            data: props.stats.ingameBanAmounts,
-            fill: false,
-            backgroundColor: "#003fc5",
-            borderColor: 'rgba(0, 63, 197, 0.2)'
-          },
+            {
+                label: t("group.status.stats.servers.info.players"),
+                data: props.stats.playerAmounts,
+                fill: false,
+                backgroundColor: "#4bc0c0",
+                borderColor: 'rgba(75,192,192, 0.2)'
+            },
+            {
+                label: t("group.status.stats.servers.info.vips"),
+                data: props.stats.vipAmounts,
+                fill: false,
+                backgroundColor: "#49297e",
+                borderColor: 'rgba(73, 41, 126, 0.2)'
+            },
+            {
+                label: t("group.status.stats.servers.info.admins"),
+                data: props.stats.adminAmounts,
+                fill: false,
+                backgroundColor: "#195f08",
+                borderColor: 'rgba(25, 95, 8, 0.2)'
+            },
+            {
+                label: t("group.status.stats.servers.info.bans"),
+                data: props.stats.ingameBanAmounts,
+                fill: false,
+                backgroundColor: "#003fc5",
+                borderColor: 'rgba(0, 63, 197, 0.2)'
+            },
         ],
     };
 
@@ -132,13 +154,13 @@ export function PlayerInfo(props) {
         <div style={{ display: "block", width: "100%" }}>
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                 <h5 style={{ textAlign: "center" }}>{t("group.status.stats.servers.info.main")}</h5>
-                <Button callback={resetZoom} name="Reset zoom"/>
+                <Button callback={resetZoom} name="Reset zoom" />
             </div>
             <Line
                 ref={chartRef}
                 data={data}
-                options={options} 
-                style={{ maxHeight: "250px", marginLeft: "20px" }} 
+                options={options}
+                style={{ maxHeight: "250px", marginLeft: "20px" }}
             />
         </div>
     )
