edb5b60092560e9b5072f463066a6baec303b51c
Zefanja Jobse <zefanjaj@gmail.com>
2022-10-25 19:52:48 +0200

    testing serviceowrkers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from './App';
 
@@ -18,3 +19,8 @@ root.render(
         </QueryClientProvider>
     </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
