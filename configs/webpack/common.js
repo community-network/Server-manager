9a22e6609d38443969b92dbcad2a3063c22ce54f
Zefanja Jobse <zefanjaj@gmail.com>
2023-11-11 09:03:16 +0100

    fix file issue

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -23,7 +23,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpe?g|svg)$/,
+        test: /\.(svg)$/,
         loader: "file-loader",
         options: {
           name: "assets/[name].[ext]",
